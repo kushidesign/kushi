55d31a86b1adfe247758004eb985d6da61616395
Jeremiah Coyle <jcoyle@gmail.com>
2022-02-12 15:53:18 -0800

    Upgrade alpha version
    Upgrade par dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.paintparty/kushi "1.0.0-alpha"
+(defproject org.clojars.paintparty/kushi "1.0.0-alpha2"
   :description         "ClojureScript CSS Solution"
   :url                 "https://github.com/paintparty/kushi"
   :license             {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,7 +6,7 @@
   :dependencies        [[org.clojure/clojure "1.10.3"]
                         [garden "1.3.10"]
                         [io.aviso/pretty "1.1"]
-                        [org.clojars.paintparty/par "1.0.0"]]
+                        [org.clojars.paintparty/par "1.1.0"]]
   :repl-options        {:init-ns kushi.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
                                     :sign-releases false}]])
