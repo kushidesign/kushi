f9ff25e498d3d388d2dee48f8e570e3ca4a2788f
Jeremiah Coyle <jcoyle@gmail.com>
2022-02-12 15:25:33 -0800

    Remove post-build parsing routine

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies        [[org.clojure/clojure "1.10.3"]
                         [garden "1.3.10"]
                         [io.aviso/pretty "1.1"]
-                        [clj-ph-css "0.1.3"]
                         [org.clojars.paintparty/par "1.0.0"]]
   :repl-options        {:init-ns kushi.core}
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
