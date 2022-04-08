065951f68c8cb68e10fd7d6f2a702f17d8569888
Jeremiah Coyle <jcoyle@gmail.com>
2022-04-08 08:54:31 -0700

    Add support for enhanced defclasses
    Add support for theming

--- src/kushi/core.clj
+++ src/kushi/core.clj
@@ -8,17 +8,17 @@
   [par.core :refer [? !? ?+ !?+]]
   [kushi.arguments :as arguments]
   [kushi.atomic :as atomic]
+  [kushi.shorthand :as shorthand]
   [kushi.config :refer [user-config]]
   [kushi.parse :as parse]
   [kushi.printing :as printing]
   [kushi.selector :as selector]
   [kushi.specs :as specs]
   [kushi.state :as state]
+  [kushi.theme :as theme]
   [kushi.stylesheet :as stylesheet]
   [kushi.typography :refer [system-font-stacks]]
   [kushi.utils :as util]))
-
-
 
 ;TODO move this to utils
 (defmacro keyed [& ks]
@@ -64,13 +64,33 @@
     #_(? (keyed classes* classes ret))
     ret))
 
+(defn- shared-classes-inj
+  [distinct-classes]
+  (let [ret* (keep #(get @state/kushi-atomic-user-classes %) distinct-classes)
+        grouped  (group-by :__classtype__ ret*)
+        ret+  (reduce (fn [acc [classtype shared-classes]]
+                       (assoc acc
+                              classtype
+                              (mapv (fn [{:keys [selector garden-vecs]}]
+                                      (let [css-injection-for-kushi-atomic (stylesheet/garden-vecs-injection garden-vecs)]
+                                        [selector css-injection-for-kushi-atomic]))
+                                    shared-classes)))
+                     {}
+                     grouped)
+
+        ret  (mapv (fn [{:keys [selector garden-vecs]}]
+                     (let [css-injection-for-kushi-atomic (stylesheet/garden-vecs-injection garden-vecs)]
+                       [selector css-injection-for-kushi-atomic]))
+                   ret*)]
+    ret+))
+
 (defn- hydrated-defclass
   [classname classtype coll*]
   (let [{:keys [selector
                 selector*]} (selector/selector-name
                              {:defclass-name classname
                               :defclass-hash atomic/defclass-hash
-                              :atomic-class? (= classtype :kushi-atomic)})
+                              :atomic-class? (contains? #{:kushi-atomic :defclass-kushi-override} classtype)})
         hydrated-styles              (parse/with-hydrated-classes coll*)
         tokenized-styles             (mapv (partial parse/kushi-style->token selector*) hydrated-styles)
         grouped-by-mqs               (parse/grouped-by-mqs tokenized-styles)
@@ -82,8 +102,15 @@
     #_(? 'ret ret)
     ret))
 
-(defn defclass* [sym coll* form-meta]
-  (let [classtype                (if (-> sym meta :kushi) :kushi-atomic :defclass)
+
+(defn defclass* [sym* coll* form-meta]
+  (let [sym                      (if (keyword? sym*) (symbol sym*) sym*)
+        classtype                (cond
+                                   (keyword? sym*)         :defclass-kushi-override
+                                   (-> sym meta :kushi)    :kushi-atomic
+                                   (-> sym meta :kushi-override) :defclass-kushi-override
+                                   (-> sym meta :override) :defclass-user-override
+                                   :else                   :defclass)
         defclass-name            (keyword sym)
         style-map-vecs           (when (-> coll* last map?)
                                    (-> coll* last style-map->vecs))
@@ -119,60 +146,66 @@
      :console-warning-args console-warning-args
      :m                    m}))
 
-; todo
-; no prefix
-; (defclass :^lit name )
+(defn defclass-noop? [sym coll*]
+  (and (nil? sym) (= coll* '(nil))))
 
 (defmacro defclass
   [sym & coll*]
-  (let [form-meta             (meta &form)
-        dupe-defclass-warning (printing/dupe-defclass-warning
-                               {:fname "defclass"
-                                :nm sym
-                                :form-meta form-meta})]
+  (when-not (defclass-noop? sym coll*)
+   (let [form-meta             (meta &form)
+         dupe-defclass-warning (printing/dupe-defclass-warning
+                                {:fname "defclass"
+                                 :nm (if (keyword? sym) (symbol sym) sym)
+                                 :form-meta form-meta})]
 
-    (printing/print-dupe2! dupe-defclass-warning)
-    (reset! state/current-macro :defclass)
+     (printing/print-dupe2! dupe-defclass-warning)
+     (reset! state/current-macro :defclass)
 
-    (let [{:keys [caching?
-                  cache-key
-                  cached]}             (state/cached :defclass sym coll*)
-          {:keys [defclass-name
-                  coll
-                  invalid-args
-                  console-warning-args
-                  m]
-           :as result}                 (or cached (defclass* sym coll* (meta &form)))
-          {:keys [garden-vecs
-                  selector]}           m
-          js-args-warning              (printing/preformat-js-warning console-warning-args)
-          garden-vecs-for-shared       (stylesheet/garden-vecs-injection garden-vecs)
-          inject?                      (:runtime-injection? user-config)]
+     (let [{:keys [caching?
+                   cache-key
+                   cached]}             (state/cached :defclass sym coll*)
+           {:keys [defclass-name
+                   coll
+                   invalid-args
+                   console-warning-args
+                   m]
+            :as result}                 (or cached (defclass* sym coll* (meta &form)))
+           {:keys [garden-vecs
+                   selector
+                   __classtype__]}      m
+           js-args-warning              (printing/preformat-js-warning console-warning-args)
+           garden-vecs-for-shared       (stylesheet/garden-vecs-injection garden-vecs)
+           inject?                      (:runtime-injection? user-config)]
 
-        (printing/ansi-bad-args-warning console-warning-args)
+       (printing/ansi-bad-args-warning console-warning-args)
 
-        ;; Put atomic class into global registry
-        (swap! state/kushi-atomic-user-classes assoc defclass-name m)
+      ;; Put atomic class into global registry
+       (swap! state/kushi-atomic-user-classes assoc defclass-name m)
+       (swap! state/ordered-defclasses conj (:n m))
 
-        (printing/diagnostics :defclass {:defclass-map m
-                                         :args         coll
-                                         :sym          sym})
+       (printing/diagnostics :defclass {:defclass-map m
+                                        :args         coll
+                                        :sym          sym})
 
-        (when caching? (swap! state/styles-cache-updated assoc cache-key result))
+       (when caching? (swap! state/styles-cache-updated assoc cache-key result))
 
-        ;; Dev-only runtime code for potential warnings (and possible dynamic injection.)
-        (if @KUSHIDEBUG
-          `(do
-            (kushi.core/inject-css* (quote ~garden-vecs-for-shared) "_kushi-rules-shared_")
-             (let [logfn# (fn [f# js-array#] (.apply js/console.warn js/console (f# js-array#)))]
-               (when (seq ~invalid-args) (logfn# cljs.core/to-array ~js-args-warning))
-               (when ~dupe-defclass-warning
-                 (logfn# cljs.core/to-array (:browser ~dupe-defclass-warning))))
-             nil)
-          `(do
-             (when ~inject?
-               (kushi.core/inject-css* (quote ~garden-vecs-for-shared) "_kushi-rules-shared_"))
-             nil)))))
+       ;; Dev-only runtime code for potential warnings (and possible dynamic injection.)
+       (if @KUSHIDEBUG
+         `(do
+            ;; Inject all defclasses for dev, even if they are not used.
+            ;; This makes them available for testing / sampling in browser devtools.
+            (kushi.core/inject-css* (quote ~garden-vecs-for-shared)
+                                    (~__classtype__ kushi.core/sheet-ids-by-type))
+            (let [logfn# (fn [f# js-array#] (.apply js/console.warn js/console (f# js-array#)))]
+              (when (seq ~invalid-args) (logfn# cljs.core/to-array ~js-args-warning))
+              (when ~dupe-defclass-warning
+                (logfn# cljs.core/to-array (:browser ~dupe-defclass-warning))))
+            nil)
+         `(do
+            (when ~inject?
+              (kushi.core/inject-css* (quote ~garden-vecs-for-shared)
+                                      (~__classtype__ kushi.core/sheet-ids-by-type)))
+            nil))))))
 
 (defmacro add-font-face
   "Example:
@@ -281,91 +314,285 @@
 (defn cssfn [& args]
   (cons 'cssfn (list args)))
 
-(defn shared-classes-inj
-  [distinct-classes]
-  (let [ret* (keep #(get @state/kushi-atomic-user-classes %) distinct-classes)
-        ret  (mapv (fn [{:keys [selector garden-vecs]}]
-                     (let [css-injection-for-kushi-atomic (stylesheet/garden-vecs-injection garden-vecs)]
-                       [selector css-injection-for-kushi-atomic]))
-                   ret*)]
-    ret))
+
+
+
 
 (defmacro sx
   [& args*]
-  #_(println "hilo")
-  (let [{:keys [caching?
-                cache-key
-                cached]
-         :as cache}                 (state/cached :sx args*)
-        form-meta                   (meta &form)
-        new-args                    (or cached
-                                        (-> args*
-                                            arguments/consolidated
-                                            (arguments/new-args form-meta)))
-        {:keys [prefixed-classlist
-                distinct-classes
-                attrs-base
-                kushi-attr
-                css-vars
-                garden-vecs
-                data-cljs]}         new-args
-        element-style-inj           (stylesheet/garden-vecs-injection garden-vecs)
-        shared-styles-inj           (shared-classes-inj distinct-classes)
+  (when-not (= args* '(nil))
+   (let [{:keys [caching?
+                 cache-key
+                 cached]
+          :as cache}                 (state/cached :sx args*)
+         form-meta                   (meta &form)
+         new-args                    (or cached
+                                         (-> args*
+                                             arguments/consolidated
+                                             (arguments/new-args form-meta)))
+         {:keys [prefixed-classlist
+                 distinct-classes
+                 attrs-base
+                 kushi-attr
+                 css-vars
+                 garden-vecs
+                 data-cljs]}         new-args
+         theme?                      (:kushi-theme? kushi-attr)
+         element-style-inj           (stylesheet/garden-vecs-injection garden-vecs)
+         shared-styles-inj           (shared-classes-inj distinct-classes)
 
         ; printing
-        _ (printing/set-warnings!)
-        warnings-js @state/warnings-js]
+         _ (printing/set-warnings!)
+         warnings-js @state/warnings-js]
 
-    (when (and caching? (not cached))
-      (swap! state/styles-cache-updated assoc cache-key new-args))
-    (state/add-styles! garden-vecs)
-    (printing/print-warnings!)
+     (when (and caching? (not cached))
+       (swap! state/styles-cache-updated assoc cache-key new-args))
+     ((if theme? state/add-theme-styles! state/add-styles!) garden-vecs)
+     (printing/print-warnings!)
 
-    #_(when (= :theme (:ident kushi-attr))
-      (?
-       (keyed
-        kushi-attr
-        attrs-base
-        shared-classes-inj
-        css-vars
-        garden-vecs
-        data-cljs
-        prefixed-classlist)))
+     #_(when true
+        (?
+         (keyed
+          kushi-attr
+          attrs-base
+          shared-classes-inj
+          css-vars
+          garden-vecs
+          data-cljs
+          prefixed-classlist)))
 
     ;;TODO - fix injection mode
-    (if @KUSHIDEBUG
+     (if @KUSHIDEBUG
        ;; dev builds
        ;; TODO move cljs.core/to-array inside fn and rename js-array# ?
-      `(let [logfn# (fn [f# js-array#]
-                      (.apply js/console.warn js/console (f# js-array#)))]
-         (do
-           (when ~warnings-js
-             (doseq [warning# ~warnings-js]
-               (logfn# cljs.core/to-array warning#)))
+       `(let [logfn# (fn [f# js-array#]
+                       (.apply js/console.warn js/console (f# js-array#)))]
+          (do
+            (when ~warnings-js
+              (doseq [warning# ~warnings-js]
+                (logfn# cljs.core/to-array warning#)))
 
-           (kushi.core/inject-style-rules (quote ~element-style-inj))
-           (kushi.core/inject-kushi-atomics ~shared-styles-inj)
+            ((if ~theme? kushi.core/inject-theme-rules kushi.core/inject-style-rules)
+             (quote ~element-style-inj))
+            (kushi.core/inject-kushi-atomics ~shared-styles-inj)
 
            ;; return attributes map for the element
-           (kushi.core/merged-attrs-map
-            ~attrs-base
-            ~prefixed-classlist
-            ~css-vars
-            ~data-cljs)))
+            (kushi.core/merged-attrs-map
+             ~attrs-base
+             ~prefixed-classlist
+             ~css-vars
+             ~data-cljs)))
 
        ;; release builds
-      (if (:runtime-injection? user-config)
-        `(do
-           (kushi.core/inject-style-rules (quote ~element-style-inj))
-           (kushi.core/inject-kushi-atomics ~shared-styles-inj)
-           (kushi.core/merged-attrs-map
-            ~attrs-base
-            ~prefixed-classlist
-            ~css-vars))
-        `(do
-           (kushi.core/merged-attrs-map
-            ~attrs-base
-            ~prefixed-classlist
-            ~css-vars))))))
+       (if (:runtime-injection? user-config)
+         `(do
+            ((if ~theme? kushi.core/inject-theme-rules kushi.core/inject-style-rules)
+             (quote ~element-style-inj))
+            (kushi.core/inject-kushi-atomics ~shared-styles-inj)
+            (kushi.core/merged-attrs-map
+             ~attrs-base
+             ~prefixed-classlist
+             ~css-vars))
+         `(do
+            (kushi.core/merged-attrs-map
+             ~attrs-base
+             ~prefixed-classlist
+             ~css-vars)))))))
+
+;; For generating sx-theme! destructuring vecs at repl
+#_(? (mapv (fn [n] [(symbol (str "c" n)) (symbol (str "c" n "m"))]) (range 1 45)))
+#_(? (mapv (fn [n] (list 'kushi.core/defclass (symbol (str "~c" n)) (symbol (str "~c" n "m")))) (range 1 45)))
+#_(? (mapv (fn [n] (symbol (str "m" n))) (range 1 45)))
+#_(? (mapv (fn [n] (list 'kushi.core/sx (symbol (str "~m" n)))) (range 1 45)))
 
 
+(defmacro sx-theme! []
+  (let [{:keys [styles toks overrides]} (theme/theme-by-compo theme/base-theme)
+        [[c1 c1m]
+         [c2 c2m]
+         [c3 c3m]
+         [c4 c4m]
+         [c5 c5m]
+         [c6 c6m]
+         [c7 c7m]
+         [c8 c8m]
+         [c9 c9m]
+         [c10 c10m]
+         [c11 c11m]
+         [c12 c12m]
+         [c13 c13m]
+         [c14 c14m]
+         [c15 c15m]
+         [c16 c16m]
+         [c17 c17m]
+         [c18 c18m]
+         [c19 c19m]
+         [c20 c20m]
+         [c21 c21m]
+         [c22 c22m]
+         [c23 c23m]
+         [c24 c24m]
+         [c25 c25m]
+         [c26 c26m]
+         [c27 c27m]
+         [c28 c28m]
+         [c29 c29m]
+         [c30 c30m]
+         [c31 c31m]
+         [c32 c32m]
+         [c33 c33m]
+         [c34 c34m]
+         [c35 c35m]
+         [c36 c36m]
+         [c37 c37m]
+         [c38 c38m]
+         [c39 c39m]
+         [c40 c40m]
+         [c41 c41m]
+         [c42 c42m]
+         [c43 c43m]
+         [c44 c44m]] (into [] overrides)
+        [m1
+         m2
+         m3
+         m4
+         m5
+         m6
+         m7
+         m8
+         m9
+         m10
+         m11
+         m12
+         m13
+         m14
+         m15
+         m16
+         m17
+         m18
+         m19
+         m20
+         m21
+         m22
+         m23
+         m24
+         m25
+         m26
+         m27
+         m28
+         m29
+         m30
+         m31
+         m32
+         m33
+         m34
+         m35
+         m36
+         m37
+         m38
+         m39
+         m40
+         m41
+         m42
+         m43
+         m44] styles
+         kushi-debug   @KUSHIDEBUG
+         rt-injection? (:runtime-injection? user-config)]
+
+    (doseq [tok toks] (state/add-custom-property! tok))
+
+    `(do
+       (kushi.core/defclass ~c1 ~c1m)
+       (kushi.core/defclass ~c2 ~c2m)
+       (kushi.core/defclass ~c3 ~c3m)
+       (kushi.core/defclass ~c4 ~c4m)
+       (kushi.core/defclass ~c5 ~c5m)
+       (kushi.core/defclass ~c6 ~c6m)
+       (kushi.core/defclass ~c7 ~c7m)
+       (kushi.core/defclass ~c8 ~c8m)
+       (kushi.core/defclass ~c9 ~c9m)
+       (kushi.core/defclass ~c10 ~c10m)
+       (kushi.core/defclass ~c11 ~c11m)
+       (kushi.core/defclass ~c12 ~c12m)
+       (kushi.core/defclass ~c13 ~c13m)
+       (kushi.core/defclass ~c14 ~c14m)
+       (kushi.core/defclass ~c15 ~c15m)
+       (kushi.core/defclass ~c16 ~c16m)
+       (kushi.core/defclass ~c17 ~c17m)
+       (kushi.core/defclass ~c18 ~c18m)
+       (kushi.core/defclass ~c19 ~c19m)
+       (kushi.core/defclass ~c20 ~c20m)
+       (kushi.core/defclass ~c21 ~c21m)
+       (kushi.core/defclass ~c22 ~c22m)
+       (kushi.core/defclass ~c23 ~c23m)
+       (kushi.core/defclass ~c24 ~c24m)
+       (kushi.core/defclass ~c25 ~c25m)
+       (kushi.core/defclass ~c26 ~c26m)
+       (kushi.core/defclass ~c27 ~c27m)
+       (kushi.core/defclass ~c28 ~c28m)
+       (kushi.core/defclass ~c29 ~c29m)
+       (kushi.core/defclass ~c30 ~c30m)
+       (kushi.core/defclass ~c31 ~c31m)
+       (kushi.core/defclass ~c32 ~c32m)
+       (kushi.core/defclass ~c33 ~c33m)
+       (kushi.core/defclass ~c34 ~c34m)
+       (kushi.core/defclass ~c35 ~c35m)
+       (kushi.core/defclass ~c36 ~c36m)
+       (kushi.core/defclass ~c37 ~c37m)
+       (kushi.core/defclass ~c38 ~c38m)
+       (kushi.core/defclass ~c39 ~c39m)
+       (kushi.core/defclass ~c40 ~c40m)
+       (kushi.core/defclass ~c41 ~c41m)
+       (kushi.core/defclass ~c42 ~c42m)
+       (kushi.core/defclass ~c43 ~c43m)
+       (kushi.core/defclass ~c44 ~c44m)
+
+       (kushi.core/sx ~m1)
+       (kushi.core/sx ~m2)
+       (kushi.core/sx ~m3)
+       (kushi.core/sx ~m4)
+       (kushi.core/sx ~m5)
+       (kushi.core/sx ~m6)
+       (kushi.core/sx ~m7)
+       (kushi.core/sx ~m8)
+       (kushi.core/sx ~m9)
+       (kushi.core/sx ~m10)
+       (kushi.core/sx ~m11)
+       (kushi.core/sx ~m12)
+       (kushi.core/sx ~m13)
+       (kushi.core/sx ~m14)
+       (kushi.core/sx ~m15)
+       (kushi.core/sx ~m16)
+       (kushi.core/sx ~m17)
+       (kushi.core/sx ~m18)
+       (kushi.core/sx ~m19)
+       (kushi.core/sx ~m20)
+       (kushi.core/sx ~m21)
+       (kushi.core/sx ~m22)
+       (kushi.core/sx ~m23)
+       (kushi.core/sx ~m24)
+       (kushi.core/sx ~m25)
+       (kushi.core/sx ~m26)
+       (kushi.core/sx ~m27)
+       (kushi.core/sx ~m28)
+       (kushi.core/sx ~m29)
+       (kushi.core/sx ~m30)
+       (kushi.core/sx ~m31)
+       (kushi.core/sx ~m32)
+       (kushi.core/sx ~m33)
+       (kushi.core/sx ~m34)
+       (kushi.core/sx ~m35)
+       (kushi.core/sx ~m36)
+       (kushi.core/sx ~m37)
+       (kushi.core/sx ~m38)
+       (kushi.core/sx ~m39)
+       (kushi.core/sx ~m40)
+       (kushi.core/sx ~m41)
+       (kushi.core/sx ~m42)
+       (kushi.core/sx ~m43)
+       (kushi.core/sx ~m44)
+
+      ;;  (when (or ~kushi-debug ~rt-injection?)
+      ;;    (kushi.core/inject-custom-properties! ~vars))
+
+       )))
