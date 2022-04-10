111484249320627e50821dae44934dd2cd439a0c
Jeremiah Coyle <jcoyle@gmail.com>
2022-04-10 13:51:33 -0700

    Add utility fns

--- src/kushi/ui/util.cljs
+++ src/kushi/ui/util.cljs
@@ -17,3 +17,42 @@
     (.select el)
     (js/document.execCommand "copy")
     (.removeChild js/document.body el)))
+
+(defn screen-quadrent
+  "Pass a dom node and get a tuple back reprenting the quadrant in which the center of the node lives.
+   (screen-quadrant (js/document.getElementById \"my-id\")) => [:top :left]"
+  [node]
+  (let [rect (.getBoundingClientRect node)
+        x* (* 0.5 (- (.-right rect) (.-left rect)))
+        x  (+ (.-right rect) x*)
+        y* (* 0.5 (- (.-bottom rect) (.-top rect)))
+        y  (+ (.-top rect) y*)
+        top? (> 0.5 (/ y js/window.innerHeight))
+        left? (> 0.5 (/ x js/window.innerWidth))]
+    [(if top? :top :bottom) (if left? :left :right)]))
+
+(defn set-overlay-position! [node parent]
+  (let [[tb lr] (screen-quadrent parent)
+        right? (= lr :right)
+        top? (= tb :top)]
+
+    ;; display on right or left
+    ;; (set! node.style.left (if right? "unset" "100%"))
+    ;; (set! node.style.right (if right? "100%" "unset"))
+    ;; (set! node.style.top (if top? 0 "unset"))
+    ;; (set! node.style.bottom (if top? "unset" 0))
+
+    ;; display on top or bottom
+    (set! node.style.left (if right? "unset" "0"))
+    (set! node.style.right (if right? "0" "unset"))
+    (set! node.style.top (if top? "100%" "unset"))
+    (set! node.style.bottom (if top? "unset" "100%"))
+    ))
+
+(defn conditional-display? [node]
+  (= "true" (.getAttribute node "data-kushi-conditional-display")))
+
+(defn toggle-boolean-attribute [node attr]
+  (let [attr-val (.getAttribute node (name attr))
+        newv (if (= attr-val "false") true false)]
+    (.setAttribute node (name attr) newv)))
