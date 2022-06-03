4a39f2b1552f5e5b821edfe1f62c0d21e5933ea0
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-03 09:27:45 -0700

    Add temp fix for grid children

--- src/kushi/ui/grid/core.cljs
+++ src/kushi/ui/grid/core.cljs
@@ -2,6 +2,7 @@
   (:require
    [kushi.core :refer (sx merge-with-style)]
    [kushi.ui.core :refer (opts+children)]
+   [par.core :refer-macros [!? ?]]
    #_[playground.util :as util :refer-macros (keyed)] ))
 
 (defn grid
@@ -12,17 +13,23 @@
          :or {column-min-width :150px
               gap :20px
               aspect-ratio 1}} opts
-        aspect-ratio-pct (str (* 100 (if (number? aspect-ratio) (js/Math.abs aspect-ratio) 1)) "%")]
-    [:section
-     (merge-with-style
-      (sx 'kushi-grid
-          {:style {:>*:w     :auto
-                   :>*:h     0
-                   :>*:pt    aspect-ratio-pct
-                   :>*:position :relative
-                   :d        :grid
-                   :gtc      (str "repeat(auto-fit, minmax(" (name column-min-width) ", 1fr))")
-                   :grid-gap gap
-                   :width    :100%}})
-      attr)
-      children]))
+        aspect-ratio-pct (str (* 100 (if (number? aspect-ratio) (js/Math.abs aspect-ratio) 1)) "%")
+        ;; TODO this unwrapping is temp fix - move upstream into opts+children
+        children-unwrapped (if (and (seq children) (= 1 (count children)) (coll? (first children)))
+                            (first children)
+                            children)]
+    (into
+     [:section
+      (merge-with-style
+       (sx 'kushi-grid
+           {:style {:>*:w        :auto
+                    :>*:h        0
+                    :>*:pt       aspect-ratio-pct
+                    :>*:position :relative
+                    :d           :grid
+                    :gtc         (str "repeat(auto-fit, minmax(" (name column-min-width) ", 1fr))")
+                    :color       :red
+                    :grid-gap    gap
+                    :width       :100%}})
+       attr)]
+     children-unwrapped)))
