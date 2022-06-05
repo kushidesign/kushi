e829cfd134f468fbb4424ad07cd1e34b57c03b99
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-05 09:44:17 -0700

    Add tooltip.events ns

---
+++ src/kushi/ui/tooltip/events.cljs
@@ -0,0 +1,15 @@
+(ns kushi.ui.tooltip.events
+  (:require-macros [kushi.utils])
+  (:require
+   [kushi.ui.tooltip.core :refer (tooltip+parent remove-tooltip! expand-tooltip!)]
+   [kushi.ui.dom :refer (conditional-display?)]))
+
+(defn tooltip-mouse-enter [%]
+  (when-let [[tooltip parent] (tooltip+parent %)]
+    (when-not (conditional-display? tooltip)
+      (expand-tooltip! tooltip parent))))
+
+(defn tooltip-mouse-leave [%]
+  (when-let [[tooltip parent] (tooltip+parent %)]
+    (when-not (conditional-display? tooltip)
+      (remove-tooltip! parent))))
