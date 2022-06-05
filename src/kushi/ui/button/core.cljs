52de4a1d4156e2c5d570b21f56546022e6ec6eb4
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-05 09:40:04 -0700

    Refactor to use label component

--- src/kushi/ui/button/core.cljs
+++ src/kushi/ui/button/core.cljs
@@ -1,44 +1,37 @@
 (ns kushi.ui.button.core
   (:require-macros
-   [kushi.core :refer (sx cssfn defclass)]
-   [kushi.ui.core :refer (defcom defcom+)])
+   [kushi.core :refer (sx)]
+   [kushi.ui.core :refer (defcom+)])
   (:require
-   [kushi.core :refer (merged-attrs-map merge-with-style)]
-   [kushi.ui.core :refer (gui)]
-   [kushi.ui.scaling :refer [icon-margin icon-font-size]]
-   [kushi.ui.tooltip.core :refer (tooltip-mouse-leave tooltip-mouse-enter)]))
+   [kushi.core :refer (merge-with-style)]
+   [kushi.ui.core :refer (opts+children)]
+   [kushi.ui.label.core :refer (label)]
+   [kushi.ui.tooltip.events :refer (tooltip-mouse-leave tooltip-mouse-enter)]))
 
+(defn button
+  [& args]
+  (let [[attrs & children] (opts+children args)]
+    [:button
+     (merge-with-style
+      (sx 'kushi-button
+          :.transition
+          :.pointer
+          {:data-kushi-ui :button})
+      attrs)
+     [apply
+      label
+      (sx
+       :padding--0.8em:1.2em
+       {:data-kushi-tooltip true
+        :aria-expanded      "false"
+        :on-mouse-enter     tooltip-mouse-enter
+        :on-mouse-leave     tooltip-mouse-leave})
+      children]]))
 
-(def label-base-sx
- (kushi.core/sx
-    'kushi-label-base:ui
-    ;; :.normal
-    {
-     :data-kushi-tooltip true
-     :aria-expanded "false"
-     :on-mouse-enter tooltip-mouse-enter
-     :on-mouse-leave tooltip-mouse-leave
-     :style {;; Remove this position relative?
-             :position                   :relative
-             :min-width                  :1rem
-             :h                          :fit-content
-             :w                          :fit-content
-             ;;  :fs                         "var(--text-medium)"
-             ;;  :&_.kushi-icon:fs           :1.1875em #_"var(--mui-icon-medium-font-size)"
-             :m                          0
-             :p                          0
-             :line-height                :normal
-             :>span:d                    :flex
-             :>span:jc                   :center
-             :>span:ai                   :center
-             :&_.kushi-label-text+.kushi-icon:mis "var(--mui-icon-margin-inline-ems)"
-             :&_.kushi-icon+.kushi-label-text:mis "var(--mui-icon-margin-inline-ems)"
-             :bgi                        :none}}))
-
-(defcom+ button
+#_(defcom+ button
   [:button
    (merge-with-style
-    label-base-sx
+    ;; label-base-sx
     (sx
      'kushi-button
      :.transition
@@ -46,6 +39,7 @@
               :cursor        :pointer}
       :id    {:data-kushi-ui    :button}})
     &attrs)
-   [:span &children]]
+   [apply label &children]
+   #_[:span &children]]
   ;; fn for processing children
   #(if (string? %) [:span.kushi-label-text %] %))
