4d11eda5ef4379d02e1f943dfb7ca383c9897109
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-29 23:59:49 -0400

    Add ceremonial code

--- src/kushi/ui/card/core.cljs
+++ src/kushi/ui/card/core.cljs
@@ -1,11 +1,20 @@
 (ns kushi.ui.card.core
-  (:require [kushi.core :refer-macros (sx)]
-            [kushi.ui.core :refer-macros (defcom)]))
+  (:require [kushi.core :refer (sx merge-with-style)]
+            [kushi.ui.core :refer (opts+children)]))
 
-(defcom card
-  [:div (sx 'kushi-card
-            :.relative
-            :w--100%
-            :p--2rem
-            :bgc--white
-            {:data-kushi-ui :card})])
+(defn card
+  "Desc for"
+  [& args]
+  (let [[opts attrs & children] (opts+children args)
+        {:keys []}              opts]
+    (into
+     [:div (merge-with-style
+            (sx 'kushi-card
+                :.relative
+                :w--100%
+                :p--2rem
+                :bgc--white
+                {:data-kushi-ui :card})
+            attrs)]
+     children)))
+
