961294d3bbddf4d1758709ae4ad638c033eca995
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-30 00:01:16 -0400

    Refactor

--- src/kushi/ui/collapse/core.cljs
+++ src/kushi/ui/collapse/core.cljs
@@ -19,8 +19,7 @@
    (sx 'kushi-collapse-body-wrapper :overflow--hidden)
    [:div:! (sx
             'kushi-collapse-body
-            :bt--1px:solid:transparent
-            :bb--1px:solid:transparent
+            :bbe--1px:solid:transparent
             :padding-block--0.25em:0.5em)]])
 
 (defn toggle-class-on-ancestor [node root-class class]
@@ -46,58 +45,89 @@
       (when-not (= open-node clicked-node)
         [open-node (-> open-node .-nextSibling)]))))
 
-(defcom collapse-header
-  (let [on-click #(let [node        (.closest (-> % .-target) "[aria-expanded][role='button']")
-                        accordian*  (util/grandparent node)
-                        accordian   (when-let [cl (util/has-class accordian* "kushi-accordian")]
-                                      accordian*)
-                        [open-node
-                         open-exp-parent] (other-expanded-node accordian node)
-                        exp-parent  (-> node .-nextSibling)
-                        exp-inner   (-> node .-nextSibling .-firstChild)
-                        exp-inner-h (outer-height exp-inner)
-                        expanded?   (util/attribute-true? node :aria-expanded)
-                        height      (str exp-inner-h "px")
-                        ->height    (if expanded? "0px" height)
-                        no-height?  (and expanded? (string/blank? exp-parent.style.height))]
-                    (when no-height? (set! exp-parent.style.height height))
-                    (js/window.requestAnimationFrame (fn []
-                                                       (when open-exp-parent
-                                                         (set! open-exp-parent.style.height "0px")
-                                                         (toggle-boolean-attribute open-node :aria-expanded))
-                                                       (set! exp-parent.style.height ->height)
-                                                       (toggle-boolean-attribute node :aria-expanded))))]
-    [:div
-     (sx
-      'kushi-collapse-header
-      :.pointer
-      {:class         [:.flex-row-fs :.collapse-header]
-       :style         {:ai                                               :center
-                       :padding-block                                    :0.75em
-                       :transition                                       :all:200ms:linear
-                       :+section:transition-property                     :height
-                       :+section:transition-timing-function              "cubic-bezier(0.23, 1, 0.32, 1)"
-                       :+section:transition-duration                     :--kushi-collapse-transition-duration
-                       "&[aria-expanded='false']:+section:height"        :0px
+(defn collapse-header
+  [& args]
+  (let [[opts attrs & children] (opts+children args)
+        {:keys [icon-position]} opts]
+    (let [on-click #(let [node                        (.closest (-> % .-target) "[aria-expanded][role='button']")
+                          accordian*                  (util/grandparent node)
+                          accordian                   (when-let [cl (util/has-class accordian* "kushi-accordian")]
+                                                        accordian*)
+                          [open-node
+                           open-exp-parent]           (other-expanded-node accordian node)
+                          exp-parent                  (-> node .-nextSibling)
+                          exp-inner                   (-> node .-nextSibling .-firstChild)
+                          exp-inner-h                 (outer-height exp-inner)
+                          expanded?                   (util/attribute-true? node :aria-expanded)
+                          height                      (str exp-inner-h "px")
+                          ->height                    (if expanded? "0px" height)
+                          no-height?                  (and expanded? (string/blank? exp-parent.style.height))]
+                      (when no-height? (set! exp-parent.style.height height))
+                      (js/window.requestAnimationFrame (fn []
+                                                         (when open-exp-parent
+                                                           (set! open-exp-parent.style.height "0px")
+                                                           (toggle-boolean-attribute open-node :aria-expanded))
+                                                         (set! exp-parent.style.height ->height)
+                                                         (toggle-boolean-attribute node :aria-expanded))))]
+      (into [:div
+             (merge-with-style
+              (sx
+               'kushi-collapse-header
+               :.pointer
+               {:class         [:.flex-row-fs :.collapse-header]
+                :style         {
+                                ;; :&_.kushi-title>.kushi-label:width              (when (= :end icon-position) :100%)
+                                :ai                                             :center
+                                :padding-block                                  :0.75em
+                                :transition                                     :all:200ms:linear
+                                :+section:transition-property                   :height
+                                :+section:transition-timing-function            "cubic-bezier(0.23, 1, 0.32, 1)"
+                                :+section:transition-duration                   :--kushi-collapse-transition-duration
+                                "&[aria-expanded='false']:+section:height"        :0px
 
-                       "&[aria-expanded='false']:+section:>*:transition" :opacity:200ms:linear:10ms
-                       "&[aria-expanded='true']:+section:>*:transition"  :opacity:200ms:linear:200ms
+                                "&[aria-expanded='false']:+section:>*:transition" :opacity:200ms:linear:10ms
+                                "&[aria-expanded='true']:+section:>*:transition"  :opacity:200ms:linear:200ms
 
-                       "&[aria-expanded='false']:+section:>*:opacity"    0
-                       "&[aria-expanded='true']:+section:>*:opacity"     1}
-       :role          :button
-       :aria-expanded false
-       :on-click      on-click})]))
+                                "&[aria-expanded='false']:+section:>*:opacity"    0
+                                "&[aria-expanded='true']:+section:>*:opacity"     1}
+                :role          :button
+                :aria-expanded false
+                :on-click      on-click})
+              attrs)]
+            children))))
 
 (defn get-attr [m k] (some-> m :parts k first))
 (defn get-children [m k] (some-> m :parts k rest))
 
-
 (defn collapse
-  "A section of content which can be collapsed and expanded"
-  [& args]
-  (let [[opts attr & children]  (opts+children args)
-        {:keys [parts expanded? on-click]} opts]
+  {:desc ["A section of content which can be collapsed and expanded."]
+   :opts '[{:name    label
+            :type    {:pred string?}
+            :default nil
+            :desc    "The text to display in the collapse header."}
+           {:name    label-expanded
+            :type    {:pred string?}
+            :default nil
+            :desc    "The text to display in the collapse header when expanded. Optional."}
+           {:name    icon
+            :type    {:pred string?}
+            :default "add"
+            :desc    ["The [name of the Material Icon](https://fonts.google.com) to use in the collapse header."
+                      "Optional."]}
+           {:name    icon-expanded
+            :type    {:pred string?}
+            :default "remove"
+            :desc    ["The [name of the Material Icon](https://fonts.google.com) to use in the collapse header when expanded."
+                      "Optional."]}
+           {:name    icon-position
+            :type    {:pred keyword? :enum [:start :end]}
+            :default :start
+            :desc    ["A value of `:start` will place the at the inline start of the header, preceding the label."
+                      "A value of `:end` will place the icon at the inline end of the header, opposite the label."
+                      "Optional."]}]}
+   [& args]
+  (let [[opts attr & children]                           (opts+children args)
+        {:keys [parts expanded? on-click icon-position]} opts]
     [:section
      (merge-with-style
       (sx
@@ -109,11 +139,23 @@
      [collapse-header
       (merge-with-style
        (:header parts)
-       (sx {:on-click on-click
-            :aria-expanded (if expanded? "true" "false")}))
-      #_[collapse-footer-contents {:label-text label-text :label-text-expanded label-text-expanded}]
+       (sx {:on-click       on-click
+            :aria-expanded  (if expanded? "true" "false")
+            :-icon-position icon-position}))
+      #_[collapse-footer-contents {:label-text          label-text
+                                   :label-text-expanded label-text-expanded}]
       [collapse-header-contents opts]]
      [collapse-body (:body parts) children]]))
 
-(defcom accordian
-  [:div.kushi-accordian])
+
+(defn accordian
+  [& args]
+  (let [[opts attrs & children] (opts+children args)
+        {:keys []}              opts]
+    (into
+     [:div
+      (merge-with-style
+       {:class [:kushi-accordian]}
+       attrs)]
+     children)))
+
