085d71e6aeeed2c1e1fa2340bf832cb856eaeaa7
Jeremiah Coyle <jcoyle@gmail.com>
2022-06-30 00:14:57 -0400

    Use defcom+ component

--- src/kushi/ui/label/core.cljs
+++ src/kushi/ui/label/core.cljs
@@ -1,22 +1,22 @@
 (ns kushi.ui.label.core
   (:require-macros
-   [kushi.core :refer (sx)])
+   [kushi.core :refer (sx)]
+   [kushi.ui.core :refer (defcom+)])
   (:require
    [kushi.ui.core :refer (opts+children)]
    [kushi.core :refer (merge-with-style)]))
 
-(defn label
-  [& args]
-  (let [[opts attrs & children] (opts+children args)
-        {:keys []}              opts]
-    (into
-     [:span
-      (merge-with-style
-       (sx 'kushi-label
-           :.flex-row-c
-           :w--fit-content
-           :>.kushi-label-text+.kushi-icon:mis--:--mui-icon-margin-inline-ems
-           :>.kushi-icon+.kushi-label-text:mis--:--mui-icon-margin-inline-ems
-           {:data-kushi-ui :label})
-       attrs)]
-     (map #(if (string? %) [:span.kushi-label-text %] %) children))))
+(defcom+ label
+  [:span
+   (merge-with-style
+    (sx 'kushi-label
+        :.flex-row-c
+        :ai--c
+        :d--inline-flex
+        :w--fit-content
+        :>.kushi-label-text+.kushi-icon:mis--:--mui-icon-margin-inline-ems
+        :>.kushi-icon+.kushi-label-text:mis--:--mui-icon-margin-inline-ems
+        {:data-kushi-ui :label})
+    &attrs)
+   &children]
+  #(if (string? %) [:span.kushi-label-text %] %))
