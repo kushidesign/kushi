60713f50735c991d81dab060bfb4e9dec5dfbecb
Jeremiah Coyle <jcoyle@gmail.com>
2022-07-30 14:07:06 -0700

    Add text casing logic

--- src/kushi/ui/tag/core.cljs
+++ src/kushi/ui/tag/core.cljs
@@ -1,20 +1,23 @@
 (ns kushi.ui.tag.core
-  (:require [kushi.core :refer [merge-with-style] :refer-macros (sx cssfn defclass)]
+  (:require [kushi.core :refer [merge-with-style] :refer-macros (sx)]
+            [kushi.ui.icon.mui.core :refer (mui-icon)]
             [kushi.ui.core :refer-macros (defcom)]))
 
 (defcom tag
-  [:div (sx :.relative)
-   [:div
-    (merge-with-style
-     (sx
-      'kushi-tag
-      :line-height--1
-      :>span:padding--0.2em:0.6em
-      :>span:max-width--140px
-      :>span:white-space--nowrap
-      :>span:overflow--hidden
-      :>span:text-overflow--ellipsis
-      :bgc--#efefef
-      {:data-kushi-ui :tag})
-     &attrs)
-    [:span (sx :d--block) &children]]])
+  (let [{:keys [dismissable?]} &opts]
+    [:div (sx :.relative)
+     [:div
+      (merge-with-style
+       (sx
+        'kushi-tag
+        :line-height--1
+        :>span:padding--0.4em:0.6em:0.4em
+        :&.lowercase:>span:padding--0.35em:0.6em:0.45em
+        :>span:max-width--140px
+        :>span:white-space--nowrap
+        :>span:overflow--hidden
+        :>span:text-overflow--ellipsis
+        :bgc--#efefef
+        {:data-kushi-ui :tag})
+       &attrs)
+      [:span (sx :d--block) &children]]]))
