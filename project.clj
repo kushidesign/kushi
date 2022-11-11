514d39dcb4c937ae3889ba91a8f763bedfe46c4a
Jeremiah Coyle <jcoyle@gmail.com>
2022-11-11 07:21:14 -0800

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.paintparty/kushi "1.0.0-a.13"
-  :description         "ClojureScript CSS Solution"
+  :description         "ClojureScript UI Framework"
   :url                 "https://github.com/paintparty/kushi"
   :license             {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
                         :url  "https://www.eclipse.org/legal/epl-2.0/"}
