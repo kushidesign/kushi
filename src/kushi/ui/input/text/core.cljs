2ef70f00425a8ecbfd0e0ec3eeacca2676f3d715
Jeremiah Coyle <jcoyle@gmail.com>
2022-11-15 22:00:46 -0800

    Support text input opts

--- src/kushi/ui/input/text/core.cljs
+++ src/kushi/ui/input/text/core.cljs
@@ -15,52 +15,82 @@
             :type    #{string vector}
             :default nil
             :desc    "A string, hiccup vector, or child component intended to aid the user and positioned within the input field area, at the end"}
-           {:name    input-attrs
+           {:name    outer-wrapper-attrs
             :type    :map
             :default nil
-            :desc    "html attributes map applied to the underlying `input` div."}]}
+            :desc    ["html attributes map applied to the div that wraps the outermost div of the component."
+                      "This div wraps the label, input-wrapper div, and the helper text span."]}
+           {:name    input-wrapper-attrs
+            :type    :map
+            :default nil
+            :desc    "html attributes map applied to the div that wraps the start-enhancer div, `input` element, and end-enhancer div."}]}
   [& args]
-  (let [
-        [opts attrs & children] (opts+children args)
-        {:keys [input-attrs
+  (let [[opts attrs & children] (opts+children args)
+        {:keys [label
+                helper
+                attrs
+                outer-wrapper-attrs
+                wrapper-attrs
                 start-enhancer
-                end-enhancer]}   opts]
-       [:div
+                end-enhancer]}   opts
+        input-id (:id attrs)]
+    [:div
+     (merge-attrs
+      (sx 'kushi-text-input-outer-wrapper
+          :ai--center)
+      outer-wrapper-attrs)
+     (when label [:label
+                  (sx 'kushi-text-input-label
+                      :.small
+                      :mbe--0.5em
+                      :d--block
+                      {:for input-id})
+                  label])
+     [:div
+      (merge-attrs
+       (sx 'kushi-text-input-wrapper
+           :.flex-row-fs
+           :ai--stretch
+           :jc--sb
+           :w--100%
+           :min-height--34px
+           :bw--1px
+           :bs--solid
+           :bc--currentColor
+           [:focus-within:bc '(rgba 0 125 250 1)])
+       wrapper-attrs)
+      (when start-enhancer
+        [:div
+         (sx 'kushi-text-input-start-enhancer
+             :d--if
+             :ai--center
+             :jc--c
+             [:pi (when (string? start-enhancer) :0.375em)])
+         start-enhancer])
+      [:div (sx 'kushi-text-input-input-wrapper :flex-grow--1)
+       [:input
         (merge-attrs
-         (sx :.flex-row-fs
-             :ai--stretch
-             :jc--sb
+         (sx 'kushi-text-input-input
+             :.transition
+             :h--100%
              :w--100%
-             :min-height--34px
-             :bw--1px
-             :bs--solid
-             :bc--black)
-         attrs)
-        (when start-enhancer
-          [:div
-           (sx 'kushi-text-input-start-enhancer
-               :d--if
-               :flex-direction--column
-               :jc--c
-               :pis--0.5em)
-           start-enhancer])
-        [:div (sx :flex-grow--1)
-         [:input
-          (merge-attrs
-           (sx 'kushi-text-input-input
-               :.transition
-               :h--100%
-               :w--100%
-               :pi--0.5em
-               :placeholder:o--0.4
-               {:type        :text
-                :placeholder "asdfhyere"})
-           input-attrs)]]
-        (when end-enhancer
-          [:div
-           (sx 'kushi-text-input-end-enhancer
-               :d--if
-               :flex-direction--column
-               :jc--c
-               :&_.kushi-mui-icon:pi--0)
-           end-enhancer])]))
+             :pi--0.5em
+             :placeholder:o--0.4
+             {:type :text})
+         attrs)]]
+      (when end-enhancer
+        [:div
+         (sx 'kushi-text-input-end-enhancer
+             :d--if
+             :ai--center
+             :jc--c
+             [:pi (when (string? end-enhancer) :0.375em)])
+         end-enhancer])]
+     (when helper
+       [:span
+        (sx 'kushi-text-input-helper
+            :d--block
+            :.small
+            :o--0.6
+            :mbs--0.5em)
+        helper])]))
