b21bfbd12db2834feed978146951a58e9936fb8d
Jeremiah Coyle <jcoyle@gmail.com>
2022-12-07 20:20:25 -0800

    remove max-width

--- src/kushi/ui/tag/core.cljs
+++ src/kushi/ui/tag/core.cljs
@@ -1,38 +1,22 @@
 (ns kushi.ui.tag.core
   (:require [kushi.core :refer [merge-attrs] :refer-macros (sx)]
             [kushi.ui.core :refer-macros (defcom)]))
+
+;; TODO enable same features as button?
 (defcom tag
   (let [{:keys [dismissable?]} &opts]
     [:div
      (merge-attrs
       (sx
        'kushi-tag
+       :.info
        :line-height--1
        :>span:padding--0.4em:0.6em:0.4em
        :&.lowercase:>span:padding--0.35em:0.6em:0.45em
-       :>span:max-width--140px
+       :>span:d--block
        :>span:white-space--nowrap
        :>span:overflow--hidden
        :>span:text-overflow--ellipsis
        {:data-kushi-ui :tag})
       &attrs)
-     [:span (sx :d--block) &children]]))
-
-#_(defcom tag
-  (let [{:keys [dismissable?]} &opts]
-    [:div (sx :.relative)
-     [:div
-      (merge-attrs
-       (sx
-        'kushi-tag
-        :line-height--1
-        :>span:padding--0.4em:0.6em:0.4em
-        :&.lowercase:>span:padding--0.35em:0.6em:0.45em
-        :>span:max-width--140px
-        :>span:white-space--nowrap
-        :>span:overflow--hidden
-        :>span:text-overflow--ellipsis
-        :bgc--#efefef
-        {:data-kushi-ui :tag})
-       &attrs)
-      [:span (sx :d--block) &children]]]))
+     [:span &children]]))
