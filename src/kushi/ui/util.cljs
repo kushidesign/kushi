98547c33ba6310293d3e040aad761e8aba7896e6
Jeremiah Coyle <jcoyle@gmail.com>
2023-04-01 23:55:11 -0700

    Add attribute function

--- src/kushi/ui/util.cljs
+++ src/kushi/ui/util.cljs
@@ -36,3 +36,8 @@
 
 (defn nameable? [x]
   (or (string? x) (keyword? x) (symbol? x)))
+
+(defn html-attr? [m k]
+  (when (keyword? k)
+   (or (true? (k m))
+       (true? ((keyword (str (name k) "?")) m)))))
