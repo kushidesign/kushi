7c2ca0a6554e2735c5fb1c9d3aab21b17bd47c13
Jeremiah Coyle <jcoyle@gmail.com>
2023-04-23 23:21:32 -0700

    Use defn

--- src/kushi/ui/tag/core.cljs
+++ src/kushi/ui/tag/core.cljs
@@ -1,22 +1,23 @@
 (ns kushi.ui.tag.core
   (:require [kushi.core :refer [merge-attrs] :refer-macros (sx)]
-            [kushi.ui.core :refer-macros (defcom)]))
+            [kushi.ui.core :refer (opts+children)]))
 
-;; TODO enable same features as button?
-(defcom tag
-  (let [{:keys [dismissable?]} &opts]
-    [:div
-     (merge-attrs
-      (sx
-       'kushi-tag
-       :.info
-       :line-height--1
-       :>span:padding--0.4em:0.6em:0.4em
-       :&.lowercase:>span:padding--0.35em:0.6em:0.45em
-       :>span:d--block
-       :>span:white-space--nowrap
-       :>span:overflow--hidden
-       :>span:text-overflow--ellipsis
-       {:data-kushi-ui :tag})
-      &attrs)
-     [:span &children]]))
+(defn tag
+  {:desc ["A tag is typically used for concise information, often in a group with other tags."]}
+  [& args]
+  (let [[_ attrs & children] (opts+children args)]
+    (into [:div
+           (merge-attrs
+            (sx
+             'kushi-tag
+             :.flex-row-c
+             :.enhanceable
+             :.info
+             :.neutral
+             :.transition
+             :line-height--1.25
+             :padding--0.4em:0.6em:0.4em
+             :&.lowercase:padding--0.35em:0.6em:0.45em
+             {:data-kushi-ui :tag})
+            attrs)]
+          children)))
