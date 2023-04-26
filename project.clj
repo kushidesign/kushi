befb38654841f4937861652b1e5227d21727664a
Jeremiah Coyle <jcoyle@gmail.com>
2023-04-26 01:17:25 -0700

    Upgrade Clojure to 1.11.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url                 "https://github.com/kushidesign/kushi"
   :license             {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
                         :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies        [[org.clojure/clojure "1.10.3"]
+  :dependencies        [[org.clojure/clojure "1.11.1"]
                         [garden "1.3.10"]
                         [applied-science/js-interop "0.3.3"]
                         [io.aviso/pretty "1.1"]
