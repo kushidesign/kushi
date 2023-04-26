6b2814bc169b1fb78d3253eb0881bed98849e699
Jeremiah Coyle <jcoyle@gmail.com>
2023-04-25 20:28:00 -0700

    Refactor alert component

--- src/kushi/ui/alert/core.cljs
+++ src/kushi/ui/alert/core.cljs
@@ -1,8 +1,6 @@
 (ns kushi.ui.alert.core
   (:require [kushi.core :refer (merge-attrs) :refer-macros [sx]]
-            [kushi.ui.dom :as dom]
             [kushi.ui.icon.core]
-            [kushi.ui.label.core :refer [label]]
             [kushi.ui.core :refer (opts+children)]))
 
 (defn alert
@@ -16,67 +14,56 @@
             :desc    ["An instance of a `kushi.ui.icon/icon` component"
                       "Places an icon anchored to the inline-start area of the alert."
                       "Optional."]}
-           {:name    close-icon?
-            :pred    boolean?
+           {:name    close-button
+            :pred    vector?
             :default nil
-            :desc    "Places an `✕` (dismiss) icon anchored to the inline-end area of the alert."}
-           {:name    close-icon-attrs
-            :pred    map?
-            :default nil
-            :desc    "Attributes map for close-icon. This is where you would put your click-handler for closing the alert."}
-          ;; Leave this out for now
-          ;;  {:name    duration
-          ;;   :pred    pos-int?
-          ;;   :default nil
-          ;;   :desc    "When supplied, the alert will dismiss itself after the given time (in milliseconds) has passed."}
+            :desc    ["Hiccup to render a close button."
+                      "Optional."]}
+
+           ;; Leave this out for now
+           ;;  {:name    duration
+           ;;   :pred    pos-int?
+           ;;   :default nil
+           ;;   :desc    "When supplied, the alert will dismiss itself after the given time (in milliseconds) has passed."}
+
            ]}
 
   [& args]
 
   (let [[opts attrs & children]    (opts+children args)
         {:keys [icon
-                close-icon?
-                close-icon-attrs
-                duration]}         opts
-        alert-id     (str (.now js/Date))]
+                close-button
+                header-text
+                ;; Leave this out for now
+                duration
+                ]}                 opts
+        alert-id                   (str (.now js/Date))]
 
     ;; Leave this out for now
     ;; (when (pos-int? duration)
     ;;   (js/setTimeout #(when-let [el (dom/el-by-id alert-id)]
     ;;                     (.remove el))
     ;;                  duration))
-
+    
     [:section
      (merge-attrs
       (sx 'kushi-alert
+          :ai--c
           :.info
-          :p--1rem
           :w--100%
           {:id alert-id
           ;;  :data-kushi-ui          :alert
           ;;  :data-kushi-ui-alert-id alert-id
            })
       attrs)
-     [:div (sx :.flex-row-fs
-               :ai--c
+     [:div (sx 'kushi-alert-header-wrap
+               :.flex-row-sb
                :.relative
+               :p--0.75rem:1rem
                :w--100%)
-      [label (sx (when-not icon :.hidden)) icon]
-      (if (every? string? children)
-        [label (sx :flex-grow--1 :ta--c) children]
-        (into [:span (sx :flex-grow--1 :ta--c)] children))
-      (when close-icon?
-        (let [hover-bgc "rgba 255 255 255 0.3"]
-          [label (merge-attrs
-                  (sx :.pointer
-                      :.pill
-                      :outline-width--3px
-                      :outline-style--style
-                      :outline-color--transparent
-                      [:outline-color hover-bgc]
-                      [:hover:bgc hover-bgc]
-                      {:tab-index      0
-                       :role           :button})
-                  close-icon-attrs)
-           ;; todo use svg
-           [kushi.ui.icon.core/icon :close]]))]]))
+      [:div (sx 'kushi-alert-header-icon-wrap :min-width--1em) icon]
+      header-text
+      [:div (sx 'kushi-alert-header-close-button-wrap :min-width--1em) close-button]]
+     (when (seq children)
+       (into [:div (sx :p--1rem)]
+             children))]))
