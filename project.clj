08e1589b1cf364544f6ec8ea2add89503a31fcd3
Jeremiah Coyle <jcoyle@gmail.com>
2024-03-03 01:40:08 -0700

    Update alpha version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject design.kushi/kushi "1.0.0-a.18"
+(defproject design.kushi/kushi "1.0.0-a.19"
   :description         "ClojureScript UI Framework"
   :url                 "https://github.com/kushidesign/kushi"
   :license             {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
