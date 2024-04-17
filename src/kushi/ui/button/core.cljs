f9e114af62ff9502a99c13fbe0f55fc24e48b888
Jeremiah Coyle <jcoyle@gmail.com>
2024-04-17 17:08:01 -0600

    Add attribute args

--- src/kushi/ui/button/core.cljs
+++ src/kushi/ui/button/core.cljs
@@ -49,6 +49,7 @@
                 :.neutral
                 :.enhanceable
                 :ai--c
+                :w--fit-content
                 [:pis pis]
                 [:pie pie]
                 :pb--$button-padding-block-ems
@@ -60,4 +61,61 @@
             (when loading? {:data-kushi-ui-progress true})
             attrs)]
           children)))
+
+(defn button2
+  {:desc ["Buttons provide cues for actions and events."
+          "These fundamental components allow users to process actions or navigate an experience."]}
+  [& args]
+  (let [[opts attrs & children] (opts+children args)
+        {:keys [loading?
+                size
+                variant
+                shape
+                semantic]
+         :or   {size     "medium"
+                variant  "soft"
+                shape    "rounded"
+                semantic "neutral"}}  opts
+        only-icons?             (every? icon-child? children)
+        icon-inline-start?      (some-> children first icon-child?)
+        icon-inline-end?        (some-> children last icon-child?)
+        bordered?               (some->> attrs :class seq (some #{:bordered "bordered"}))
+        pi-opts                 (keyed only-icons? bordered?)
+        pis                     (resolve-inline-offset (assoc pi-opts :icon-inline-*? icon-inline-start?))
+        pie                     (resolve-inline-offset (assoc pi-opts :icon-inline-*? icon-inline-end?))]
+
+    (into [:button
+           (merge-attrs
+            (sx 'kushi-button
+                :.flex-row-c
+                :.transition
+                :.pointer
+                :.relative
+                ;; :.neutral
+                :.enhanceable
+                :ai--c
+                :w--fit-content
+                [:pis pis]
+                [:pie pie]
+                :pb--$button-padding-block-ems
+                [:&.bordered:pb "calc(var(--button-padding-block-ems) - var(--button-border-width))"]
+                [:&.bordered:pb "calc(var(--button-padding-block-ems) - var(--button-border-width))"]
+                {:data-kushi-ui       :button
+                 :data-kushi-size     size
+                 :data-kushi-variant  variant
+                 :data-kushi-shape    shape
+                 :data-kushi-semantic semantic
+                 :aria-busy           loading?
+                 :aria-label          (when loading? "loading")
+                 :class [size variant shape semantic]})
+            (when loading? {:data-kushi-ui-progress true})
+            attrs)]
+          children)))
+
+
+
+
+
+
+
 
