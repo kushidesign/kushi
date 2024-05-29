547a72886a9dfb4b103350dfaa068b2043548398
Jeremiah Coyle <jcoyle@gmail.com>
2024-05-29 16:47:50 -0600

    alert -> callout

--- src/kushi/ui/callout/core.cljs
+++ src/kushi/ui/callout/core.cljs
@@ -1,13 +1,13 @@
-(ns kushi.ui.alert.core
+(ns kushi.ui.callout.core
   (:require [kushi.core :refer (merge-attrs) :refer-macros [sx]]
             [kushi.ui.core :refer (opts+children)]
             [kushi.ui.icon.core]))
 
-(defn alert
-  {:desc ["Alerts provide contextual feedback information for the user"
+(defn callout
+  {:desc ["callouts provide contextual feedback information for the user"
           :br
           :br
-          "To position the alert at the top of the viewport, use the "
+          "To position the callout at the top of the viewport, use the "
           "`:.fixed-block-start-inside` utility class, or the "
           "`:.fixed-block-end-inside` utility class for positioning "
           "at the bottom of the viewport."]
@@ -16,7 +16,7 @@
             :default nil
             :desc    ["An instance of a `kushi.ui.icon/icon` component"
                       "Places an icon anchored to the inline-start area "
-                      "of the alert. Optional."]}
+                      "of the callout. Optional."]}
            {:name    close-button
             :pred    vector?
             :default nil
@@ -25,13 +25,13 @@
            {:name    header-text
             :pred    string
             :default nil
-            :desc    ["The header text to render in the alert."
+            :desc    ["The header text to render in the callout."
                       "Optional."]}
           ;;  Leave this out for now
           ;;   {:name    duration
           ;;    :pred    pos-int?
           ;;    :default nil
-          ;;    :desc    ["When supplied, the alert will dismiss itself after "
+          ;;    :desc    ["When supplied, the callout will dismiss itself after "
           ;;              "the given time (in milliseconds) has passed."]}
 
            ]}
@@ -45,33 +45,34 @@
                 ;; Leave this out for now
                 duration
                 ]}opts
-        alert-id                   (str (.now js/Date))]
+        callout-id                   (str (.now js/Date))]
 
     ;; Leave this out for now
     ;; (when (pos-int? duration)
-    ;;   (js/setTimeout #(when-let [el (domo/el-by-id alert-id)]
+    ;;   (js/setTimeout #(when-let [el (domo/el-by-id callout-id)]
     ;;                     (.remove el))
     ;;                  duration))
 
     [:section
      (merge-attrs
-      (sx 'kushi-alert
+      (sx 'kushi-callout
           :ai--c
           :.info
           :w--100%
-          {:id alert-id
-          ;;  :data-kushi-ui          :alert
-          ;;  :data-kushi-ui-alert-id alert-id
+          {:id callout-id
+          ;;  :data-kushi-ui          :callout
+          ;;  :data-kushi-ui-callout-id callout-id
            })
       attrs)
-     [:div (sx 'kushi-alert-header-wrap
+     [:div (sx 'kushi-callout-header-wrap
                :.flex-row-sb
                :.relative
+               :gap--0.5em
                :p--0.85em:0.75em
                :w--100%)
-      [:div (sx 'kushi-alert-header-icon-wrap :min-width--1em) icon]
+      [:div (sx 'kushi-callout-header-icon-wrap :min-width--1em) icon]
       header-text
-      [:div (sx 'kushi-alert-header-close-button-wrap :min-width--1em) close-button]]
+      [:div (sx 'kushi-callout-header-close-button-wrap :min-width--1em) close-button]]
      (when (seq children)
        (into [:div (sx :p--1rem)]
              children))]))
--- src/kushi/ui/callout/demo.cljs
+++ src/kushi/ui/callout/demo.cljs
@@ -1,4 +1,4 @@
-(ns kushi.ui.alert.demo
+(ns kushi.ui.callout.demo
   (:require
    [kushi.ui.icon.core :refer [icon]]
    [kushi.ui.icon.mui.svg :as mui.svg]
@@ -6,37 +6,36 @@
    [kushi.playground.component-examples :as component-examples]
    [kushi.playground.util :refer-macros [sx-call]]
    [kushi.ui.button.core :refer [button]]
-   [kushi.ui.alert.core :refer [alert]]))
+   [kushi.ui.callout.core :refer [callout]]))
 
 
-(declare alert-examples)
+(declare callout-examples)
 
-(def alert-sizes
+(def callout-sizes
   [:xxsmall
    :xsmall
    :small
    :medium
-   :large
-   :xlarge])
+   :large])
 
 (defn demo
   [component-opts]
   (into [:<>]
         (for [
-              ;; example-opts (take 5 alert-examples)
-              example-opts alert-examples
-              ;; example-opts (keep-indexed (fn [idx m] (when (contains? #{1} idx) m)) alert-examples)
+              ;; example-opts (take 5 callout-examples)
+              example-opts callout-examples
+              ;; example-opts (keep-indexed (fn [idx m] (when (contains? #{1} idx) m)) callout-examples)
               ]
           [component-examples/examples-section component-opts example-opts])))
 
-(def alert-examples
+(def callout-examples
   (let [row-attrs (sx :&_.playground-component-example-row-instance-code:w--100%
-                      :flex-direction--column )]
+                      :flex-direction--column)]
 
-    [{:desc      "Showing sizes from xxsmall to xlarge, in positive variant"
+    [{:desc      "Showing sizes from xxsmall to large, in positive variant"
       :row-attrs row-attrs
       :variants- [:filled :bordered]
-      :examples  (for [sz alert-sizes]
+      :examples  (for [sz callout-sizes]
                    {:label (name sz)
                     :attrs {:-header-text "Your transaction was successful."
                             :-icon        [icon :check-circle]
@@ -45,12 +44,12 @@
      {:desc      "With icon and dismiss button"
       :row-attrs row-attrs
       :variants- [:filled :bordered]
-      :examples  [{:label "With icon and dismiss button"
-                   :code  (sx-call [alert
+      :examples  [{
+                   :code  (sx-call [callout
                                     (sx
                                      :.positive
                                      {:-icon         [icon :info]
-                                      :-header-text  "Alert header text goes here."
+                                      :-header-text  "Your transaction was successful."
                                       :-close-button [button
                                                       (sx
                                                        :.pill
@@ -61,28 +60,28 @@
 
      {:desc      "Neutral variant"
       :row-attrs row-attrs
-      :examples  [{:label "neutral"
+      :examples  [{
                    :attrs  {:-header-text "Your transaction was successful."
                             :-icon        [icon :info]
                             :class        ["neutral"]}}]}
 
      {:desc      "Accent variant"
       :row-attrs row-attrs
-      :examples  [{:label "accent"
+      :examples  [{
                    :attrs  {:-header-text "Please check out the new features."
                             :-icon        [icon :info]
                             :class        ["accent"]}}]}
 
      {:desc      "Positive variant"
       :row-attrs row-attrs
-      :examples  [{:label "positive"
+      :examples  [{
                    :attrs  {:-header-text "Please check out the new features."
                             :-icon        [icon :check-circle]
                             :class        ["positive"]}}]}
 
      {:desc      "Warning variant"
       :row-attrs row-attrs
-      :examples  [{:label "warning"
+      :examples  [{
                    :attrs  {:-header-text "Please check out the new features."
                             :-icon        [icon :warning]
                             :class        ["warning"]}}]}
@@ -90,7 +89,7 @@
 
      {:desc      "Negative variant"
       :row-attrs row-attrs
-      :examples  [{:label "negative"
+      :examples  [{
                    :attrs  {:-header-text "Please check out the new features."
                             :-icon        [icon :error]
                             :class        ["negative"]}}]}
