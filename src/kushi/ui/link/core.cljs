762b352080a54a47e6f47e4153fafea9fd4e35b1
Jeremiah Coyle <jcoyle@gmail.com>
2024-06-13 15:28:02 -0600

    Update link component

--- src/kushi/ui/link/core.cljs
+++ src/kushi/ui/link/core.cljs
@@ -1,21 +1,20 @@
 (ns kushi.ui.link.core
   (:require [kushi.core :refer (merge-attrs) :refer-macros (sx)]
-            [kushi.ui.core :refer (opts+children)]
-            [kushi.ui.label.core :refer (label)]))
+            [kushi.ui.core :refer (opts+children)]))
 
 (defn link
   "Desc for"
   [& args]
   (let [[opts attrs & children] (opts+children args)
         {:keys []}              opts]
-    [:a
-     (merge-attrs
-      (sx 'kushi-link
-          :.pointer
-          :d--ib
-          :>span:jc--fs
-          :td--underline
-          :tup--under
-          {:data-kushi-ui :link})
-      attrs)
-     [apply label children]]))
+    (into [:a
+           (merge-attrs
+            (sx 'kushi-link
+                :.pointer
+                :td--underline
+                :tup--under
+                [:tdc "color-mix(in oklch, currentColor 50%, transparent)"]
+                [:hover:tdc :currentColor]
+                {:data-kushi-ui :link})
+            attrs)]
+          children)))
