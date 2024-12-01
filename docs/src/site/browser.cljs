8eeb9ac5b84234d8245479a7f4010d5837fd2d2d
Jeremiah Coyle <jcoyle@gmail.com>
2024-11-30 22:56:57 -0700

    Create button matrix

--- docs/src/site/browser.cljs
+++ docs/src/site/browser.cljs
@@ -1,13 +1,36 @@
 (ns site.browser
   (:require
-   [kushi.core :refer [inject!]]
+   [kushi.core :refer [inject! sx]]
+   [kushi.ui.button.core :refer [button]]
    [site.theme]
    [reagent.dom :as rdom]
-   [site.views :as views]))
+   [site.views :as views]
+   [clojure.string :as string]))
+
+(def labels [[:accent "play ▸"]
+             [:positive "confirm"]
+             [:warning "proceed"]
+             [:negative "cancel"]])
+
+(defn button-group [{:keys [surface]}]
+  (into [:div (sx :.flex-row-c :gap--1rem)] 
+        (for [[colorway label] labels] 
+          [button
+           {:data-kui-colorway colorway
+            :data-kui-surface  surface}
+           (string/capitalize (name label))])))
+
+;; POC for new theming system
+(defn new-theming-system-button-example []
+  (into [:div (sx :.flex-col-c :.absolute-centered :gap--1rem)]
+        (for [surface [:outline :solid :soft :minimal]]
+          [button-group {:surface surface}])))
 
 (defn ^:dev/after-load mount-root []
   (let [root-el (.getElementById js/document "app")]
-    (rdom/render [views/main-view] root-el)))
+    (rdom/render [new-theming-system-button-example]
+                 #_[views/main-view]
+                 root-el)))
 
 (defn init []
   (mount-root))
