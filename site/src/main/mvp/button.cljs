3bd291f9b057861cce93d3e6309c95aed154d822
Jeremiah Coyle <jcoyle@gmail.com>
2024-12-03 12:21:18 -0700

    Add test code

--- site/src/main/mvp/button.cljs
+++ site/src/main/mvp/button.cljs
@@ -1,7 +1,9 @@
 (ns mvp.button
   (:require 
-   [kushi.css.core :refer [sx]]))
+   [kushi.css.core :refer [sx defcss]]))
 
+(defcss "@layer kushi-ui-theming .wttf"
+  :c--gold :bgc--red)
 
 (defn my-button []
   [:button (sx :bgc--blue :c--white) "Hello!"])
