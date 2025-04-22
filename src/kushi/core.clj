99fcce23fd5ee9d233fa5ddb6920bb925c1f82ce
Jeremiah Coyle <jcoyle@gmail.com>
2025-04-22 11:07:33 -0400

    Add soft classic

--- src/kushi/core.clj
+++ src/kushi/core.clj
@@ -943,9 +943,35 @@
 (defn colorway-selector [s]
   (str "[data-kushi-colorway=\"" s "\"]"))
 
+(defn soft-classic [s]
+  {:bgc               (keyword (str "$background-color-" s "-soft-3"))
+   :dark:bgc          (keyword (str "$background-color-" s "-soft-3-dark-mode"))
+   :hover:bgc         (keyword (str "$background-color-" s "-soft-2"))
+   :dark:hover:bgc    (keyword (str "$background-color-" s "-soft-2-dark-mode"))
+   :active:bgc        (keyword (str "$background-color-" s "-soft"))
+   :dark:active:bgc   (keyword (str "$background-color-" s "-soft-dark-mode"))
+   :color             (keyword (str "$foreground-color-" s "-3"))
+   :hover:color       (keyword (str "$foreground-color-" s "-3"))
+   :active:color      (keyword (str "$foreground-color-" s "-3"))
+   :dark:color        (keyword (str "$foreground-color-" s "-3-dark-mode"))
+   :dark:hover:color  (keyword (str "$foreground-color-" s "-3-dark-mode"))
+   :dark:active:color (keyword (str "$foreground-color-" s "-3-dark-mode"))})
 
 (defn colorway-args [s]
-  [{:color                             (keyword (str "$foreground-color-" s ))
+  (let [soft
+        {:bgc               (keyword (str "$background-color-" s "-soft-3"))
+         :dark:bgc          (keyword (str "$background-color-" s "-soft-3-dark-mode"))
+         :hover:bgc         (keyword (str "$background-color-" s "-soft-2"))
+         :dark:hover:bgc    (keyword (str "$background-color-" s "-soft-2-dark-mode"))
+         :active:bgc        (keyword (str "$background-color-" s "-soft"))
+         :dark:active:bgc   (keyword (str "$background-color-" s "-soft-dark-mode"))
+         :color             (keyword (str "$foreground-color-" s "-3"))
+         :hover:color       (keyword (str "$foreground-color-" s "-3"))
+         :active:color      (keyword (str "$foreground-color-" s "-3"))
+         :dark:color        (keyword (str "$foreground-color-" s "-3-dark-mode"))
+         :dark:hover:color  (keyword (str "$foreground-color-" s "-3-dark-mode"))
+         :dark:active:color (keyword (str "$foreground-color-" s "-3-dark-mode"))}]
+   [{:color                             (keyword (str "$foreground-color-" s ))
     :hover:color                       (keyword (str "$foreground-color-" s "-2"))
     :active:color                      (keyword (str "$foreground-color-" s "-3"))
     :dark:color                        (keyword (str "$foreground-color-" s "-dark-mode"))
@@ -956,20 +982,49 @@
     :dark:hover:bgc                    (keyword (str "$background-color-" s "-soft-dark-mode"))
     :dark:active:bgc                   (keyword (str "$background-color-" s "-soft-2-dark-mode"))
 
-    "[data-kushi-surface= \"soft\"]"       {:bgc             (keyword (str "$background-color-" s "-soft"))
-                                            :dark:bgc        (keyword (str "$background-color-" s "-soft-dark-mode"))
-                                            :hover:bgc       (keyword (str "$background-color-" s "-soft-2"))
-                                            :dark:hover:bgc  (keyword (str "$background-color-" s "-soft-2-dark-mode"))
-                                            :active:bgc      (keyword (str "$background-color-" s "-soft-3"))
-                                            :dark:active:bgc (keyword (str "$background-color-" s "-soft-3-dark-mode"))}
 
-    "[data-kushi-surface= \"solid\"]"      {:bgc        (keyword (str "$background-color-" s "-hard"))
-                                            :hover:bgc  (keyword (str "$background-color-" s "-hard-2"))
-                                            :active:bgc (keyword (str "$background-color-" s "-hard-3"))}
+    "[data-kushi-surface= \"faint\"]"       
+    {:bgc             (keyword (str "$background-color-" s "-soft"))
+     :dark:bgc        (keyword (str "$background-color-" s "-soft-dark-mode"))
+     :hover:bgc       (keyword (str "$background-color-" s "-soft-2"))
+     :dark:hover:bgc  (keyword (str "$background-color-" s "-soft-2-dark-mode"))
+     :active:bgc      (keyword (str "$background-color-" s "-soft-3"))
+     :dark:active:bgc (keyword (str "$background-color-" s "-soft-3-dark-mode"))}
 
-    "dark:[data-kushi-surface= \"solid\"]" {:bgc        (keyword (str "$background-color-" s "-hard-dark-mode"))
-                                            :hover:bgc  (keyword (str "$background-color-" s "-hard-2-dark-mode"))
-                                            :active:bgc (keyword (str "$background-color-" s "-hard-3-dark-mode"))}}])
+
+    "[data-kushi-surface= \"soft\"]"
+    soft
+
+
+    "[data-kushi-surface= \"soft-classic\"]"
+    (merge soft
+           {:--classic-trim-color (keyword (str "$" s "-150"))
+            :--classic-trim-color-dark (keyword (str "$" s "-800"))})
+
+
+    "[data-kushi-surface= \"solid\"]"      
+    {:bgc        (keyword (str "$background-color-" s "-hard"))
+     :hover:bgc  (keyword (str "$background-color-" s "-hard-2"))
+     :active:bgc (keyword (str "$background-color-" s "-hard-3"))}
+
+
+    "[data-kushi-surface= \"solid-classic\"]"    
+    {:--classic-trim-color (keyword (str "$" s "-550"))
+     :bgc                  (keyword (str "$background-color-" s "-hard"))
+     :hover:bgc            (keyword (str "$background-color-" s "-hard-2"))
+     :active:bgc           (keyword (str "$background-color-" s "-hard-3"))}
+
+
+    "dark:[data-kushi-surface= \"solid-classic\"]" 
+    {:bgc        (keyword (str "$background-color-" s "-hard-dark-mode"))
+     :hover:bgc  (keyword (str "$background-color-" s "-hard-2-dark-mode"))
+     :active:bgc (keyword (str "$background-color-" s "-hard-3-dark-mode"))}
+
+
+    "dark:[data-kushi-surface= \"solid\"]" 
+    {:bgc        (keyword (str "$background-color-" s "-hard-dark-mode"))
+     :hover:bgc  (keyword (str "$background-color-" s "-hard-2-dark-mode"))
+     :active:bgc (keyword (str "$background-color-" s "-hard-3-dark-mode"))}}]))
 
 
 (defmacro ^:public css-rule
