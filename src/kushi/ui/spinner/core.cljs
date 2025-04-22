dbea2c510a17e2a555a6999fbe71817eb254f7fe
Jeremiah Coyle <jcoyle@gmail.com>
2025-04-22 12:25:39 -0400

    Update spinner component

--- src/kushi/ui/spinner/core.cljs
+++ src/kushi/ui/spinner/core.cljs
@@ -11,6 +11,16 @@
   [:0% {:opacity 1}]
   [:50% {:opacity 0}])
 
+(defcss "kushi-spinner-wrapper"
+  :position--relative
+  :pi--0.3em
+  :d--inline-flex
+  :flex-direction--row
+  :jc--c
+  :ta--center
+  :ai--c
+  :min-width--$loading-spinner-height)
+
 (defn thinking [& args]
   (let [[_ attrs & _]
         (extract args thinking)
@@ -18,87 +28,71 @@
         circle
         [:div (sx ".kushi-pulsing-dot"
                   :.pill
-                  :w--0.29em
-                  :h--0.29em
+                  :w--0.3em
+                  :h--0.3em
                   :bgc--currentColor
                   [:animation "var(--spinner-animation-duration) linear infinite pulsing"]
                   ["nth-child(2):animation-delay" "calc(var(--spinner-animation-duration) / 4)"]
                   ["nth-child(3):animation-delay" "calc(var(--spinner-animation-duration) / 2)"])]]
-    [:div (merge-attrs
-           {:class       (css
-                          ".kushi-spinner-thinking"
-                          :.flex-row-c
-                          :gap--0.333em)
-            :aria-hidden true}
-           attrs)
-     circle
-     circle
-     circle]))
+
+    [:div {:data-kushi-spinner ""
+           :class (css ".kushi-thinking-wrapper"
+                       :.kushi-spinner-wrapper
+                       :.transition)} 
+     [:div (merge-attrs
+            {:class       (css
+                           ".kushi-thinking"
+                           :.flex-row-c
+                           :gap--0.333em)
+             :aria-hidden true}
+            attrs)
+      circle
+      circle
+      circle]]))
 
 (defn propeller [& args]
   (let [[_ attrs & _] (extract args propeller)]
-    [:div (merge-attrs
-           {:class       (css
-                          ".kushi-spinner-propeller"
-                          [:animation
-                           "var(--spinner-animation-duration) linear infinite spin"]
-                          [:b
-                           "max(0.055em, 1px) solid currentColor"]
-                          :h--$loading-spinner-height
-                          :w--0px)
-            :aria-hidden true}
-           attrs)]))
+    [:div {:data-kushi-spinner ""
+           :class (css ".kushi-propeller-wrapper"
+                       :.kushi-spinner-wrapper
+                       :.transition
+                       :pi--0.5em)}
+     [:div (merge-attrs
+            {:class       (css ".kushi-propeller"
+                               [:animation
+                                "var(--spinner-animation-duration) linear infinite spin"]
+                               [:b
+                                "max(0.055em, 1px) solid currentColor"]
+                               :h--$loading-spinner-height
+                               :w--0px)
+             :aria-hidden true}
+            attrs)]]))
 
 (defn donut [& args]
   (let [[_ attrs & _] (extract args donut)]
-    [:div (merge-attrs
-           {:class       (css
-                          ".kushi-spinner-donut"
-                          :position--relative
-                          :.before-absolute-fill
-                          :.after-absolute-fill
-                          [:animation
-                           "var(--spinner-animation-duration) linear infinite spin"]
-                          [:before:bw "max(2.5px, 0.125em)"]
-                          [:after:bw "max(2.5px, 0.125em)"]
-                          :w--$loading-spinner-height
-                          :h--$loading-spinner-height
-                          :before:border-radius--9999px
-                          :before:bs--solid
-                          :before:bc--transparent
-                          :before:bbsc--currentColor
-                          :after:border-radius--9999px
-                          :after:o--0.2
-                          :after:bs--solid
-                          :after:bc--currentColor)
-            :aria-hidden true}
-           attrs)]))
-
-(defn spinner
-  {:summary "Spinners indicate the in-progress status of an operation such as
-             loading or processing."
-   :desc "The spinner is component is meant to be used in conjuction with a
-          component such as `spinner.core/donut`,  `spinner.core/propeller`, or
-          `spinner.core/propeller`. See the \"Usage with a button\" in the
-          Spinner > Examples section."}
-  [& args]
-  (let [{:keys [opts attrs children]} (extract args spinner)
-        [content component] children]
-    [:span
-     (merge-attrs
-      (sx ".kushi-spinner-wrapper" :position--relative)
-      attrs)
-     [:span
-      (sx ".kushi-spinner-content"
-          ["has-ancestor([data-kushi-ui-spinner='true']):visibility" :hidden])
-      content]
-     [:div
-      (sx ".kushi-spinner"
-          :.absolute-centered
-          :d--none
-          ["has-ancestor([data-kushi-ui-spinner='true']):d" :block])
-      (cond
-        (fn? component)
-        [component]
-        (coll? component)
-        component)]]))
+    [:div {:data-kushi-spinner ""
+           :class (css ".kushi-donut-wrapper"
+                       :.kushi-spinner-wrapper
+                       :.transition)}
+     [:div (merge-attrs
+            {:class       (css
+                           ".kushi-donut"
+                           :position--relative
+                           :.before-absolute-fill
+                           :.after-absolute-fill
+                           [:animation
+                            "var(--spinner-animation-duration) linear infinite spin"]
+                           [:before:bw "max(2.5px, 0.125em)"]
+                           [:after:bw "max(2.5px, 0.125em)"]
+                           :w--$loading-spinner-height
+                           :h--$loading-spinner-height
+                           :before:border-radius--9999px
+                           :before:bs--solid
+                           :before:bc--transparent
+                           :before:bbsc--currentColor
+                           :after:border-radius--9999px
+                           :after:o--0.2
+                           :after:bs--solid
+                           :after:bc--currentColor)
+             :aria-hidden true}
+            attrs)]]))
