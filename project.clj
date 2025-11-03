dc8c8fd5781f07b8ac03391f6fd6d93b5eebde51
Jeremiah Coyle <jcoyle@gmail.com>
2025-11-02 21:40:54 -0500

    Update deps

--- project.clj
+++ project.clj
@@ -12,12 +12,15 @@
                         [org.clojure/clojure "1.11.1"]
                         [applied-science/js-interop "0.3.3"]
                         [expound "0.9.0"]
-                        [io.github.paintparty/bling "0.8.4"]
-                        [io.github.paintparty/fireworks "0.12.3"]
+                        [io.github.paintparty/bling "0.8.8"]
+                        [io.github.paintparty/fireworks "0.12.6"]
                         [me.flowthing/pp "2024-01-04.60"]
                         [babashka/process "0.5.22"]
                         [borkdude/edamame "1.4.27"]
-                        ;; [org.flatland/ordered "1.15.12"]
+
+                        ;; temp
+                        [org.flatland/ordered "1.15.12"]
+
                         ;; for testing & profiling
                         [com.taoensso/tufte "2.6.3"]
                         [metosin/malli "0.18.0"]]
